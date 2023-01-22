b18c7e317775def2e5c242d889d38ec154eadcb0
tyrannosaurus-lee <miaow106@gmail.com>
2023-01-22 20:20:31 +0900

    4.20 useEffect 제대로 사용하기 : syntaxerror 오류 미해결

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Products () {
-    const [count, setCount] = useState(0);
     const [products, setProducts] = useState([]);
+    const [checked, setChecked] = useState(false);
+    const handleChange = () => setChecked(prev => !prev);
 
     // 컴포넌트가 처음 등록되었을때 처리해야하는 일이 있다면 
     // component안에서 하면 무한루프에 빠짐 -> useEffect안에서 하기
     useEffect(() => {
-        fetch('data/products.json')
+        // fetch('data/products.json')
+        // checked가 되었다면 sale_을 붙여주고 아니라면 아무것도 붙이지 않고 products.json을 받아온다.
+        fetch(`data/${checked ? 'sale_' : ''}products.json`)
             .then((res) => res.json())
             .then((data) => {
                 console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
@@ -15,12 +18,20 @@ export default function Products () {
             });
             // useEffect의 반환하는 콜백함수 등록
             // ⬇️이 콜백함수는 이 컴포넌트가 없어질때(화면에서 사라질때 Unmount될때 호출되는 함수)
-            return ()=> {
+            return () => {
                 console.log('🧹깨끗하게 청소하는 일들을 합니다.')
             }
-        }, []); // 딱 한번만 처리되어야 한다면 두번째 배열을 텅텅 비게 만들면 됨 
+    //}, []); // 딱 한번만 처리되어야 한다면 두번째 배열을 텅텅 비게 만들면 됨 
+    }, [checked]); // checked가 변경이 될때마다 useEffect가 다시 실행되어야 한다.
     return (
         <>
+            <input 
+                id="checkbox" 
+                type="checkbox" 
+                value={checked} 
+                onChange={handleChange}
+            />
+            <label htmlFor="checkbox">Show Only 🔥 Sale</label>
             <ul>
                 {products.map((product) => (
                     <li key={product.id}>
@@ -31,7 +42,7 @@ export default function Products () {
                     </li>
                 ))}
             </ul>
-            <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+            {/* <button onClick={() => setCount((prev) => prev + 1)}>{count}</button> */}
         </>
     )
 }
