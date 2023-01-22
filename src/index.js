bc75d289b8d83ab0fa0375d9654b2c456aed3e4a
tyrannosaurus-lee <miaow106@gmail.com>
2023-01-22 15:14:10 +0900

    Revert "4.16 내부 상태관리 State < 기본 내용들 정리"
    This reverts commit 097787f6e7be4bf711bf29b4c827f0a65966d219.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-// import AppProfile from './AppProfile';
-import AppCounter from './AppCounter';
+import AppProfile from './AppProfile';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <AppProfile /> */}
-    <AppCounter />
+    <AppProfile />
   </React.StrictMode>
 );
 
